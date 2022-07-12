460e12b14ab51c88070982fe0540076342302e98
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-07-12 14:44:13 -0300

    refactor: modularizando variáveis

--- client/client-grpc.js
+++ client/client-grpc.js
@@ -1,6 +1,8 @@
 const grpc = require('grpc');
 const PROTO_PATH = '../elements.proto';
+const local_server = require('../utils/global_constants');
+
 const ElementService = grpc.load(PROTO_PATH).ElementService;
 
-const client = new ElementService('localhost:5051', grpc.credentials.createInsecure());
+const client = new ElementService(local_server, grpc.credentials.createInsecure());
 module.exports = client
